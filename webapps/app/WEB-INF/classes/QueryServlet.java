0c2b3f20a95b2ebdbb2f30628457fbc95c06829e
Lhanze De Jesus <lhanzedejesus01@gmail.com>
2021-10-05 21:59:53 +0800

    Json

--- webapps/app/WEB-INF/classes/QueryServlet.java
+++ webapps/app/WEB-INF/classes/QueryServlet.java
@@ -2,31 +2,39 @@ import java.io.*;
 import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;@WebServlet("/query")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
-public class QueryServlet extends HttpServlet {   // The doGet() runs once per HTTP GET request to this servlet.
+import javax.servlet.annotation.*;
+
+@WebServlet("/query")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class QueryServlet extends HttpServlet {
+    // The doGet() runs once per HTTP GET request to this servlet.
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Set the MIME type for the response message
         response.setContentType("text/html");
         // Get a output writer to write the response message into the network socket
-        PrintWriter out = response.getWriter();      // Print an HTML page as the output of the query
+        PrintWriter out = response.getWriter();
+        // Print an HTML page as the output of the query
         out.println("<html>");
         out.println("<head><title>Query Response</title></head>");
-        out.println("<body>");      try (
+        out.println("<body>");
+        try (
                 // Step 1: Allocate a database 'Connection' object
                 Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                        "jdbc:mysql://localhost:3306/bank?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
                         "root", "lhanzedj01");   // For MySQL
-                // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"         // Step 2: Allocate a 'Statement' object in the Connection
+                // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+                // Step 2: Allocate a 'Statement' object in the Connection
                 Statement stmt = conn.createStatement();
         ) {
             // Step 3: Execute a SQL SELECT query
             String sqlStr = "select * from books where author = "
                     + "'" + request.getParameter("author") + "'"   // Single-quote SQL string
-                    + " and qty > 0 order by price desc";         out.println("<h3>Thank you for your query.</h3>");
+                    + " and qty > 0 order by price desc";
+            out.println("<h3>Thank you for your query.</h3>");
             out.println("<p>Your SQL statement is: " + sqlStr + "</p>"); // Echo for debugging
-            ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server         // Step 4: Process the query result set
+            ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server
+            // Step 4: Process the query result set
             int count = 0;
             while(rset.next()) {
                 // Print a paragraph <p>...</p> for each record
